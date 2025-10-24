ecce70784dc20c007de070befa3474f3535c2522
herneshem <ernestoami@hotmail.com>
2025-10-24 10:47:11 +0200

    prueba newuser

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user';
 import { App } from './app';
 import { Sala1 } from './components/sala1/sala1';
+import { NewuserComponent } from './components/newuser/newuser.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-    { path: 'actor', component: Sala1},
 
-  {
-  path: 'user', component: UserComponent}
+  { path: 'user', component: UserComponent },
+
+  { path: 'newuser', component: NewuserComponent },
+  { path: 'actor', component: Sala1 }
 
 ];
---
+++ src/app/components/newuser/newuser.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Servform } from '../../services/servform';
+
+@Component({
+  selector: 'app-newuser',
+  imports: [ReactiveFormsModule],
+  templateUrl: './newuser.component.html',
+  styles: ``
+})
+export class NewuserComponent {
+  name = new FormControl('');
+ private userServ = inject(Servform);
+  private formBuilder = inject(FormBuilder);
+
+  inscription = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    passw: ['', [Validators.required]],
+    confirm: ['', [Validators.required]]
+  })
+
+    newhandleSend() {
+
+    if (this.inscription.valid) {
+      const data = this.inscription.value;
+      console.log('Enviando datos:', data);
+
+      this.userServ.createUser(data).subscribe({
+        next: (response) => {
+          console.log('Respuesta del backend:', response);
+        },
+        error: (err) => {
+          console.error('Error al enviar:', err);
+        }
+      });
+    } else {
+      console.warn('Formulario inválido');
+    }
+    console.log(this.inscription)
+  }
+}
